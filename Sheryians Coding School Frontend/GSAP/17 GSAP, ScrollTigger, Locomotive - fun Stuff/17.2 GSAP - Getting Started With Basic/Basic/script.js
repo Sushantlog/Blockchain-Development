66b3a53a178791384905f16a7b7c252e9ba48147
Sushantlog <sushantbpatil53@gmail.com>
2024-11-13 19:03:52 +0530

    first commit

---
+++ Sheryians Coding School Frontend/GSAP/17 GSAP, ScrollTigger, Locomotive - fun Stuff/17.2 GSAP - Getting Started With Basic/Basic/script.js
@@ -0,0 +1,15 @@
+// gsap.to("#box",{
+//     x:1000,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     duration:2,
+//     delay:1
+// })
+
+gsap.from("#box",{
+    x:1000,
+    rotate:360,
+    backgroundColor:"blue",
+    duration:2,
+    delay:1
+})
