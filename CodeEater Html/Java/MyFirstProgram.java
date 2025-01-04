9d519499f9cd78c6975c99aaff88a603777ee3ad
Sushantlog <sushantbpatil53@gmail.com>
2025-01-04 23:05:22 +0530

    Today First commit

---
+++ CodeEater Html/Java/MyFirstProgram.java
@@ -0,0 +1,27 @@
+// class MyFirstProgram{
+//     public static void main(String args[]){
+//         System.out.println("Hello First Program");
+//     }
+// }
+
+// This Program run only in the cmd because I am running the .class file not entire file 
+// if i run full file in vscode the it will give an error => Error: Could not find or load main class MyFirstProgram       
+// Caused by: java.lang.ClassNotFoundException: MyFirstProgram 
+
+class MyFirstMG{
+    public static void main(String args[]){
+        System.out.println("Hello First Program");
+    }
+}
+
+class MyFirstClass{
+    public static void main(String[] args) {
+        System.out.println("Hello My First Class");
+    }
+}
+
+class MySecondClass {
+    public static void main(String[] args) {
+        System.out.println("Hello My Second Class");
+    }
+}
