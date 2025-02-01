5c5437e03d8b9d828446600360afe242cff9779c
Sushantlog <sushantbpatil53@gmail.com>
2025-02-01 10:30:12 +0530

    first com

---
+++ CodeEater Html/Java/MyCode.java
@@ -0,0 +1,9 @@
+public class MyCode {
+    public static void main(String[] args) {
+        short z = 32767;
+        short y = (short)32768;
+        System.out.println(z);
+        System.out.println(y);
+        // it the range is completed then it will return back to the -32768 to 32767
+    }
+}
