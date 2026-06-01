bbf79ee27bc13fea1d966a8850d2c30d95a4011e
Sushantlog <sushantbpatil53@gmail.com>
2026-06-01 23:20:45 +0530

    first commit

---
+++ Sheryians Cohort/_03_JavaScript/_14_Introduction to JavaScript/script.js
@@ -0,0 +1,78 @@
+console.log("Hello Sushant");
+
+console.warn("This is a warning");
+
+console.error("This is an error");
+
+// variables in JavaScript
+var stdName = "Sushant";
+// var a ;  //declardion
+// a = 10;  //Intaialzation
+// console.log(a);
+// var b = 20;
+// a = 15; //re-initialization
+// console.log(a);
+
+// console.log(a);
+// console.log(stdName);
+// console.log(a,b);
+
+var print = "Aniket";
+var print2 = "Pavan";
+console.log(print,print2);
+
+
+// Arithemtic Operators
+var num1 = 10;
+var num2 = 5;           
+
+console.log("The value of num1 + num2 is ", num1 + num2);
+
+
+// ------------------
+var groom = "Sushant";
+var bride = "________";
+console.log(groom,"weds",bride);
+
+// ------------------
+
+alert("are you sure you want to proceed?");
+
+// ------------------------------
+
+confirm("Are You 18+");
+prompt("Enter Your Name : ");
+
+// ---------------------------------
+
+var ans = confirm("Are You 18+");
+console.log(ans);
+
+// ---------------------------------
+
+var name = prompt("Enter Your Name : ");
+console.log(name);  // name is underline define this is keyword in js
+
+// -----------------------
+
+// let and const
+
+const num = 10;
+
+// num = 15; // you cannot change the value of const variable
+console.log(num);
+
+var num3 = 20;
+console.log(num3);
+
+// let a = 30;
+// console.log(a);
+// console.log(window);
+
+if(10>5){
+    // var a = 100;
+    let a = 100;
+    // console.log(a);  // let is block scoped
+}
+console.log(a);  // var is function scoped
+
